a8c95dda40a30bd23daf1af6121929526488fdcf
Walker <walker.m.oliver@gmail.com>
2022-08-15 11:47:09 -0400

    Add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,6 +7,12 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Al Dente Restaurant",
+      favicon: "../src/img/icon.png",
+    }),
+  ],
   module: {
     rules: [
       {
